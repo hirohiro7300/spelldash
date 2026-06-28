8c9131620f9d8e820b03ea9a97ee1af20c0efe7d
hirohiro7300 <hirohiromoto73@gmail.com>
2026-06-28 12:16:57 +0900

    Fix Supabase key

--- js/supabase.js
+++ js/supabase.js
@@ -1,10 +1,7 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const supabaseUrl = "https://sujvgwozsnzjsjmkcrnk.supabase.co";
 
-const supabaseKey = "ここに sb_publishable_... を貼る";
+const supabaseKey = "sb_publishable_から始まるキーをここに貼る";
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
-);
+export const supabase = createClient(supabaseUrl, supabaseKey);
